749495d41840f31cee401c7ac1edb79cc5064d5c
n.zinovev <nervous9339@gmail.com>
2025-01-23 22:22:31 +0300

    init app

---
+++ src/main/java/com/example/openai/chatgpt/OpenAiChatgptApplication.java
@@ -0,0 +1,13 @@
+package com.example.openai.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiChatgptApplication.class, args);
+	}
+
+}
