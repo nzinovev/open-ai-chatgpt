22d8e050fce989e85539cd080fbef97158393ff0
n.zinovev <nervous9339@gmail.com>
2025-06-01 23:50:16 +0300

    generated strategy for operationId added

--- src/main/java/com/example/openai/chatgpt/entity/Operation.java
+++ src/main/java/com/example/openai/chatgpt/entity/Operation.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -27,6 +29,7 @@ import lombok.NoArgsConstructor;
 public class Operation {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "operation_id", nullable = false)
     private Long operationId;
 
